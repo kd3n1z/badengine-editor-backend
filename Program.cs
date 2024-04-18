f7f0804339424738e257232a89db7506eb41d806
Denis Komarkov <denis.komarkov@gmail.com>
2024-04-18 23:45:18 +0300

    feat: create temporary commands to test editor

--- Program.cs
+++ Program.cs
@@ -11,6 +11,20 @@ internal static class Program {
             case "version":
                 Console.WriteLine(Version);
                 return;
+            case "watch":
+                Console.WriteLine("pretending to watch " + args[1]);
+                while (true) {
+                    Thread.Sleep(1000);
+                }
+                return;
+            case "build":
+                Console.WriteLine("pretending to build " + args[1]);
+                Thread.Sleep(4000);
+                return;
+            case "play":
+                Console.WriteLine("pretending to start " + args[1]);
+                Thread.Sleep(2000);
+                return;
             default:
                 Console.WriteLine("unknown command: `" + args[0] + "`");
                 return;
